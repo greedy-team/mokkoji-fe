9bb912f8273c01adffddf061f1bcbee37b14f63b
신혁수 <122273080+sins051301@users.noreply.github.com>
2025-03-01 15:12:03 +0900

    fix: env

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import envCompatible from "vite-plugin-env-compatible";
 
-// https://vite.dev/config/
 export default ({ mode }: { mode: string }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const env = loadEnv(mode, process.cwd(), "");
+
   return defineConfig({
     plugins: [svgr(), react(), envCompatible()],
     server: {
       proxy: {
         "/api": {
-          target: process.env.VITE_API_URL,
+          target: env.VITE_API_URL,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ""),
           secure: false,
