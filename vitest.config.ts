d3103723f96328bfdc42c9ef9b24d3a7fe34ae27
Jae Kyung Bang <qkdworud0056@gmail.com>
2025-03-06 16:18:43 +0900

    Feat: 모집공고 테스트 코드

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,9 @@ import { resolve } from "path";
 
 export default defineConfig({
   test: {
+    globals: true,
     environment: "happy-dom", 
+    setupFiles: "src/setupTests.ts", 
     alias: {
       "@": resolve(__dirname, "./src"), // '@'를 src 폴더로 설정(vite.config와 일치)
     },
