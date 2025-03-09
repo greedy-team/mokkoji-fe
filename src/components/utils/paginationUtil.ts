5747d13a0f95f82a95f48eccb3aa8352f4ae43d3
ChangwooJ <wjdckddn99@naver.com>
2025-03-09 23:05:35 +0900

    Feat: 페이지네이션 로직 변경&가이드 버튼 추가

--- src/components/utils/paginationUtil.ts
+++ src/components/utils/paginationUtil.ts
@@ -1,23 +1,24 @@
 export function generatePageNumbers(totalPages: number, currentPage: number): number[] {
     const pageNumbers = [];
-
-    if (totalPages < 6) {
-        for (let i = 1; i <= totalPages; i++) {
-            pageNumbers.push(i);
-        }
-    } else if (currentPage < 4) {
-        for (let i = 1; i <= 5; i++) {
-            pageNumbers.push(i);
-        }
-    } else if (currentPage > totalPages - 3) {
-        for (let i = totalPages - 4; i <= totalPages; i++) {
+    const pageUnit = Math.floor((currentPage - 1) / 5);
+    if(totalPages > pageUnit * 5 + 5) {
+        for(let i = pageUnit * 5 + 1; i <= pageUnit * 5 + 5; i++) {
             pageNumbers.push(i);
         }
     } else {
-        for (let i = currentPage - 2; i <= currentPage + 2; i++) {
+        for(let i = pageUnit * 5 + 1; i <= totalPages; i++) {
             pageNumbers.push(i);
         }
     }
 
     return pageNumbers;
 }
+
+export function getOtherPage(currentPage: number, totalPages: number, direction: "prev" | "next"): number {
+    if (direction === "prev" && currentPage > 5) {
+      return Math.floor((currentPage - 1) / 5) * 5 - 4;
+    } else if (direction === "next" && currentPage < totalPages) {
+      return Math.floor((currentPage - 1) / 5) * 5 + 6;
+    }
+    return currentPage;
+  }
